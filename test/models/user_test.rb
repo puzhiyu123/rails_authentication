f7b0dfa861e3bed38c205a793adc23933f3abd9b
George Pu <georgepu45@gmail.com>
2017-07-25 02:41:57 +0000

    added migrations and tests, full email authentication

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,5 +17,35 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
-
+  test "emial should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+ test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "email should be a valid email address form" do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org
+    first.last@foo.jp alice+bob@baz.cn]
+    
+    valid_addresses.each do |valid_addresses|
+      @user.email = valid_addresses
+      assert @user.valid?, "#{valid_addresses.inspect} should be valid"
+    end
+  end
 end
